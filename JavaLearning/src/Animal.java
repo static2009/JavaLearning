5a995c3a04bcf6ea2538d0fe256c193d0fa2137e
mason_000 <mason_000@limecoke>
2016-08-20 21:40:10 -0700

    Starting with the encompassing Animal Class

--- JavaLearning/src/Animal.java
+++ JavaLearning/src/Animal.java
@@ -1,5 +1,26 @@
-
 public class Animal {
-	public void 
+	
+	//Class Variables
+	String animalType;
+	String animalBreed;
+	Double animalAge;
+	Double animalWeight;
+	Double animalHeight;
+	
+	public Animal() {
+		animalType = "missing";
+		animalBreed = "missing";
+	}
+	
+	public Animal(String info) {
+		this.animalType = info;
+	}
 
+	
+	public static void main(String[] args) {
+		Animal animal1 = new Animal("Bear");
+		System.out.println(animal1);
+		
+	}
+	
 }
